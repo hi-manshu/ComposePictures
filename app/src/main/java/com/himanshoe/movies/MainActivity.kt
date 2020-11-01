e72c8f2ff02ca02248cd74da962dd65fd715af42
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-11-01 11:41:39 +0530

    project init

---
+++ app/src/main/java/com/himanshoe/movies/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.himanshoe.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.himanshoe.movies.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/himanshoe/movies/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.himanshoe.movies.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
