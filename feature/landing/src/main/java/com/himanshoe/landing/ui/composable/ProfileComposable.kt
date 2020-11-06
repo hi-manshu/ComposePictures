841c10e4655b80979f6a10a8ade371bfa5512b26
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-11-06 12:06:36 +0530

    task - edit UI + description UI done

--- feature/landing/src/main/java/com/himanshoe/landing/ui/composable/ProfileComposable.kt
+++ feature/landing/src/main/java/com/himanshoe/landing/ui/composable/ProfileComposable.kt
@@ -4,10 +4,7 @@ import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.preferredSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -15,7 +12,10 @@ import androidx.compose.material.ButtonConstants
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,6 +33,7 @@ import java.util.*
 @Composable
 fun ProfileUI(viewModel: LandingViewModel) {
     val user = viewModel.user.observeAsState().value
+    val descriptionState = remember { mutableStateOf(user?.description) }
     ScrollableColumn(modifier = Modifier.fullScreen().padding(16.dp)) {
         CoilImage(
             model = user?.displayUrl ?: "",
@@ -64,30 +65,10 @@ fun ProfileUI(viewModel: LandingViewModel) {
             color = Color.Gray,
             modifier = Modifier.drawOpacity(0.4F).padding(top = 16.dp, bottom = 16.dp)
         )
-        Column(
-            modifier = Modifier.clip(RoundedCornerShape(10.dp))
-                .align(Alignment.End)
-                .background(color = "fdecd2".toColor())
-                .fillMaxWidth()
-                .clickable(onClick = {
-                    viewModel.openEditScreen(user?.source)
-                })
-        ) {
-            Text(
-                text = "Complete Profile",
-                modifier = Modifier.align(Alignment.Start).padding(start = 16.dp, top = 20.dp),
-                style = MaterialTheme.typography.h5,
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp
-            )
-            Text(
-                text = "Click here to edit",
-                modifier = Modifier.align(Alignment.Start)
-                    .padding(start = 16.dp, top = 10.dp, bottom = 20.dp),
-                style = MaterialTheme.typography.body1,
-                fontSize = 14.sp
-            )
+        setDescription(descriptionState) {
+            viewModel.openEditScreen(user?.source)
         }
+
         Button(
             onClick = {
                 viewModel.logout()
@@ -108,5 +89,53 @@ fun ProfileUI(viewModel: LandingViewModel) {
                 style = MaterialTheme.typography.body1
             )
         }
+    }
+}
+
+@Composable
+fun setDescription(descriptionState: State<String?>, onClick: () -> Unit) {
+    val value: String? = descriptionState.value ?: ""
+    if (value?.isEmpty()!!) {
+        editCard(title = "Complete Profile", onClick = onClick)
+    } else {
+        Text(
+            text = """" $value """",
+            modifier = Modifier.clip(RoundedCornerShape(10.dp))
+                .background(color = "e2e2e2".toColor())
+                .padding(16.dp)
+                .fillMaxWidth(),
+            style = MaterialTheme.typography.body1
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        editCard(title = "Edit Profile", onClick = onClick)
+
+
+    }
+}
+
+@Composable
+private fun editCard(title: String, onClick: () -> Unit) {
+    Column(
+        modifier = Modifier.clip(RoundedCornerShape(10.dp))
+            .background(color = "fdecd2".toColor())
+            .fillMaxWidth()
+            .clickable(onClick = {
+                onClick()
+            })
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.align(Alignment.Start).padding(start = 16.dp, top = 20.dp),
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        )
+        Text(
+            text = "Click here to edit",
+            modifier = Modifier.align(Alignment.Start)
+                .padding(start = 16.dp, top = 10.dp, bottom = 20.dp),
+            style = MaterialTheme.typography.body1,
+            fontSize = 14.sp
+        )
     }
 }
